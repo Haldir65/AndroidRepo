dcc31179857bc0fdca2007fd0f53ffe9e875fbc3
Harris <mjw090608@gmail.com>
2016-09-14 22:14:03 +0800

    Material implementation

--- app/src/main/java/com/me/harris/androidanimations/MainActivity.java
+++ app/src/main/java/com/me/harris/androidanimations/MainActivity.java
@@ -2,14 +2,19 @@ package com.me.harris.androidanimations;
 
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.me.harris.androidanimations.apidemo.BouncingBalls;
+import com.me.harris.androidanimations.apidemo.MaterialWitness;
+import com.me.harris.androidanimations.apidemo.ShadowCardDrag;
 import com.me.harris.androidanimations.databinding.ActivityMainBinding;
 import com.me.harris.androidanimations.drawableAnimations.DrawableAnimationActivity;
 import com.me.harris.androidanimations.propertyanimations.PropertyAnimationActivity;
 import com.me.harris.androidanimations.utils.ActionCallBack;
+import com.me.harris.androidanimations.utils.ToastUtils;
 import com.me.harris.androidanimations.viewAnimations.ViewAnimationActivity;
 
 public class MainActivity extends AppCompatActivity implements ActionCallBack {
@@ -28,18 +33,29 @@ public class MainActivity extends AppCompatActivity implements ActionCallBack {
         switch (view.getId()) {
             case R.id.button1:
                 intent.setClass(this, ViewAnimationActivity.class);
-                startActivity(intent);
                 break;
             case R.id.button2:
                 intent.setClass(this, DrawableAnimationActivity.class);
-                startActivity(intent);
                 break;
             case R.id.button3:
                 intent.setClass(this, PropertyAnimationActivity.class);
-                startActivity(intent);
+                break;
+            case R.id.button4:
+                intent.setClass(this, BouncingBalls.class);
+                break;
+            case R.id.button5:
+                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+                    ToastUtils.showTextShort(this,"Shadow only effect on Api 21 above !");
+                    return;
+                }
+                intent.setClass(this, ShadowCardDrag.class);
+                break;
+            case R.id.button6:
+                intent.setClass(this, MaterialWitness.class);
                 break;
             default:
                 break;
         }
+        startActivity(intent);
     }
 }
