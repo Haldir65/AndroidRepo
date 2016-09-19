c596c3a36600abea320d9df4d30ed1574d67d4f4
Harris <mjw090608@gmail.com>
2016-09-19 20:46:34 +0800

    Drawbitmap method region by region

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # AndroidAnimations
-## references
+Android Graphic和Animation的一些整理
+
+
+## references 
+[AndroidNotes](https://github.com/GcsSloop/AndroidNote)关于Canvas的写的非常用心的md文档<br/>
+[Android动画](http://blog.csdn.net/yegongheng/article/details/38435553)ValueAnimator和ObjectAnimator的使用
+[伸手党系列](http://www.jianshu.com/p/13e975622b57)进阶教程，较为全面<br/>
+感谢以上作者
+
+###Todo
 
 ~~~~
---
+++ app/src/main/java/com/me/harris/androidanimations/canvas/CanvasPicture.java
@@ -0,0 +1,55 @@
+package com.me.harris.androidanimations.canvas;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Picture;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Harris on 2016/9/19.
+ */
+
+public class CanvasPicture extends View {
+
+    // 1.创建Picture
+    private Picture mPicture = new Picture();
+
+
+    // 2.录制内容方法
+    private void recording() {
+        // 开始录制 (接收返回值Canvas)
+        Canvas canvas = mPicture.beginRecording(500, 500);
+        // 创建一个画笔
+        Paint paint = new Paint();
+        paint.setColor(Color.BLUE);
+        paint.setStyle(Paint.Style.FILL);
+
+        // 在Canvas中具体操作
+        // 位移
+        canvas.translate(250,250);
+        // 绘制一个圆
+        canvas.drawCircle(0,0,100,paint);
+
+        mPicture.endRecording();
+    }
+
+    public CanvasPicture(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        recording();
+    }
+
+    public CanvasPicture(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawPicture(mPicture,new RectF(0,0,mPicture.getWidth(),200));
+
+    }
+}
