10d581d64bcff6ae4ac06c17f1a1d414365e01df
Ferimi <mjw090608@gmail.com>
2017-02-22 22:56:40 +0800

    sleepy

--- app/src/main/java/com/me/harris/androidanimations/_26_min_height/TestMinHeightActivity.java
+++ app/src/main/java/com/me/harris/androidanimations/_26_min_height/TestMinHeightActivity.java
@@ -2,16 +2,34 @@ package com.me.harris.androidanimations._26_min_height;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.Toolbar;
+import android.widget.LinearLayout;
 
 import com.me.harris.androidanimations.BaseAppCompatActivity;
+import com.me.harris.androidanimations.R;
 
 /**
  * Created by Fermi on 2017/2/22.
  */
 
 public class TestMinHeightActivity extends BaseAppCompatActivity {
+
+    private Toolbar mToolbar;
+    private LinearLayout mLlBottom;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_min_height);
+
+
+
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mLlBottom = (LinearLayout) findViewById(R.id.ll_bottom);
+
+
+
+
     }
 }
