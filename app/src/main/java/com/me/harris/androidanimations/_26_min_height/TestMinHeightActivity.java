864f3a99d630f482684dbe0b3848dfd3f99e7729
Ferimi <mjw090608@gmail.com>
2017-02-22 22:18:38 +0800

    eating dinner at 22:00 dummy!

---
+++ app/src/main/java/com/me/harris/androidanimations/_26_min_height/TestMinHeightActivity.java
@@ -0,0 +1,17 @@
+package com.me.harris.androidanimations._26_min_height;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.me.harris.androidanimations.BaseAppCompatActivity;
+
+/**
+ * Created by Fermi on 2017/2/22.
+ */
+
+public class TestMinHeightActivity extends BaseAppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
