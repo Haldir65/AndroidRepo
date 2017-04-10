73114f60ae93a9d21ef6aa765bf5546d2496ce0b
Haldir65 <mjw090608@gmail.com>
2017-04-10 11:48:58 +0800

    gradient added

--- app/src/main/java/com/me/harris/androidanimations/_01_drawable/fragment/GradientDrawableFragment.java
+++ app/src/main/java/com/me/harris/androidanimations/_01_drawable/fragment/GradientDrawableFragment.java
@@ -1,12 +1,10 @@
 package com.me.harris.androidanimations._01_drawable.fragment;
 
 import android.databinding.DataBindingUtil;
-import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -37,10 +35,16 @@ public class GradientDrawableFragment extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        mDrawable = new GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT,
-                new int[] {ContextCompat.getColor(getActivity(),R.color.md_red_900), Color.MAGENTA, Color.BLUE, Color.CYAN, Color.GREEN, Color.YELLOW, Color.RED});
+       /* mDrawable = new GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT,
+                new int[] {ContextCompat.getColor(getActivity(),R.color.md_red_900),
+                        ContextCompat.getColor(getActivity(),R.color.md_red_900),
+                        ContextCompat.getColor(getActivity(),R.color.md_red_400),
+                        ContextCompat.getColor(getActivity(),R.color.md_deep_orange_500),
+                        ContextCompat.getColor(getActivity(),R.color.md_yellow_700),
+                        ContextCompat.getColor(getActivity(),R.color.md_green_500),
+                        ContextCompat.getColor(getActivity(),R.color.md_green_900)});
         mDrawable.setCornerRadii(new float[]{10,10,10,10,10,10,20,20});
-        binding.rainBow.setBackground(mDrawable);
+        binding.rainBow.setBackground(mDrawable);*/
         binding.textLevel.setTranslationX(0);
         binding.textLevel.setText("My Text");
         binding.textLevel.animate().
