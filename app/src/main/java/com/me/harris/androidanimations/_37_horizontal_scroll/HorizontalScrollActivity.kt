e9a14532322ece77a36550dd6c9321cf3e1f4da5
Ferimi <mjw090608@gmail.com>
2017-06-11 23:23:05 +0800

    android o api is final

---
+++ app/src/main/java/com/me/harris/androidanimations/_37_horizontal_scroll/HorizontalScrollActivity.kt
@@ -0,0 +1,16 @@
+package com.me.harris.androidanimations._37_horizontal_scroll
+
+import android.os.Bundle
+import com.me.harris.androidanimations.BaseAppCompatActivity
+
+/**
+ * Courtesy of https://github.com/adityagohad/HorizontalPicker
+ */
+class HorizontalScrollActivity : BaseAppCompatActivity {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+
+
+}
