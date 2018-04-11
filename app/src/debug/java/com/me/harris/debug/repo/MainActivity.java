b21eb1001af69a2977892b5932c89f0488a86b6d
Haldir65 <mjw090608@gmail.com>
2018-04-11 11:42:56 +0800

    splitSourceSets

---
+++ app/src/debug/java/com/me/harris/debug/repo/MainActivity.java
@@ -0,0 +1,16 @@
+package com.me.harris.debug.repo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.me.harris.androidanimations.R;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_debug_main);
+
+    }
+}
