2af1fa8a2722018681698624987e85bb702a8bd3
Haldir65 <mjw090608@gmail.com>
2018-10-07 21:54:59 +0800

    prep bugly support

---
+++ _010_bugly_tinker/app/src/main/java/com/me/harris/fixsolution/App.kt
@@ -0,0 +1,10 @@
+package com.me.harris.fixsolution
+
+import android.app.Application
+import com.tencent.tinker.loader.app.TinkerApplication
+import com.tencent.tinker.loader.shareutil.ShareConstants
+
+class App : TinkerApplication(ShareConstants.TINKER_ENABLE_ALL, "com.me.harris.fixsolution.SampleApplicationLike",
+        "com.tencent.tinker.loader.TinkerLoader", false) {
+
+}
